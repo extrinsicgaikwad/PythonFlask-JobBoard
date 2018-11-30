b80d7362651c4e38b796724f4d10b45c032bcb20
extrinsicgaikwad <45346815+extrinsicgaikwad@users.noreply.github.com>
2018-11-30 18:01:04 +0530

    changed

--- jobs/app.py
+++ jobs/app.py
@@ -1,14 +1,14 @@
 import sqlite3
 from flask import Flask, render_template, g
 
-PATH ='db/jobs.sqlite'
+PATH = 'db/jobs.sqlite'
 
 app = Flask(__name__)
 
 def open_connection():
     connection = getattr(g, '_connection', None)
     if connection == None:
-        connection = g._connection = sqlite3.connect (PATH)
+        connection = g._connection = sqlite3.connect(PATH)
     connecion.row_factory = sqlite3.Row
     return _connection
 
@@ -18,14 +18,14 @@ def execute_sql(sql, values=(), commit=False, single=False):
     if commit == True:
         results = connection.commit()
     else:
-        results = connection.fetchone() if single else cursor.fetchall()
+        results = cursor.fetchone() if single else cursor.fetchall()
     cursor.close()
     return results
 
 @app.teardown_appcontext
 def close_connection(exception):
-    connection =getattr(g, '_connection', None)
-    if connection not None:
+    connection = getattr(g, '_connection', None)
+    if connection is not None:
         connection.close()
 
 
